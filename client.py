2e90b559bd111a6e5e4cccd30964f3cfd88512fd
Devashish Dubal <128912006+devashishdubal@users.noreply.github.com>
2025-03-05 17:00:00 +0530

    modifications

--- client.py
+++ client.py
@@ -2,14 +2,36 @@ import socket
 import selectors
 import types
 import sys
+import hashlib
 
 KEY = {"public":3,"private":4}#PUBLIC, PRIVATE
 
-def encrypt(message,key = ""):
+def encrypt_public(message,key = ""):
     return message
 
-def decrypt(message,key = ""):
+def encrypt_private(message,key = ""):
     return message
+
+def decrypt_private(message,key = ""):
+    return message
+
+def decrypt_public(message,key = ""):
+    return message
+
+def hash_mesg(mesg):
+    return hashlib.sha256(mesg).hexdigest()
+
+def create_message(mesg):
+    hash = hash_mesg(mesg)
+    mesg = mesg + encrypt_private(hash,KEY["private"])
+    return mesg
+
+def derive_message(text):
+    mesg_length = int(text[:4])
+    mesg = text[4:mesg_length+4]
+    hash = text[mesg_length+4:]
+    return mesg,hash
+
 
 def send_data(conn,mesg):
     print("=========")
@@ -56,6 +78,8 @@ try:
             if (key.data) is None:
                 #stdin
                 mesg = input("")
+                mesg = create_message(mesg)
+                mesg = encrypt_public(mesg,PUB_C1)
                 send_data(lsock,mesg)
             else:
                 text = recv_data(lsock,'str')
@@ -64,6 +88,10 @@ try:
                     sel.unregister(key.fileobj)
                     key.fileobj.close()
                     continue
+                text = decrypt_private(text,KEY["private"])
+                mesg,e_hash = derive_message(text)
+                recv_hash = decrypt_public((e_hash,PUB_C1))
+                hash = hash_mesg(mesg)
                 print(text)
 except:
     print("Interrupted, exiting")
